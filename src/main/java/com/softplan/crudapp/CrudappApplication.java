3687b0f0d6b099647d09ed478d25a172a7ffbf74
Davy Fonseca <davyfonseca@outlook.com>
2023-06-02 23:07:28 -0300

    Melhoria de código

--- src/main/java/com/softplan/crudapp/CrudappApplication.java
+++ src/main/java/com/softplan/crudapp/CrudappApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@EnableJpaAuditing ( auditorAwareRef = "auditingService" )
+@EnableJpaAuditing ( auditorAwareRef = "auditListener" )
 public class CrudappApplication {
 
 	public static void main(String[] args) {
